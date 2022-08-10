1ce28429e417f758d52a1cafdd21cff24ddd55eb
ramiro <ramiro.cabral@alu.ing.unlp.edu.ar>
2022-08-10 18:07:38 -0300

    Agregado ejercicio de binary search

---
+++ imperativo/clase2-recursion/5-busquedaBinaria/busquedaBinaria.pas
@@ -0,0 +1,101 @@
+program VectorOrdenado;
+
+const
+    dimF = 8;  {Dimensi�n f�sica del vector}
+
+type
+
+    vector = array [1..dimF] of integer;
+
+    dim = 0..dimF;
+
+{-----------------------------------------------------------------------------
+CARGARVECTORORDENADO - Carga ordenadamente nros aleatorios entre 0 y 100 en el
+vector hasta que llegue el nro 99 o hasta que se complete el vector}
+
+Procedure cargarVectorOrdenado ( var vec: vector; var dimL: dim);
+var
+   d, pos, j: integer;
+begin
+    Randomize;  { Inicializa la secuencia de random a partir de una semilla}
+    dimL := 0;
+    d:= random(100);
+    while (d <> 99)  and ( dimL < dimF ) do begin
+       pos:= 1;
+       while (pos <= dimL) and (vec[pos]< d) do pos:= pos + 1;
+       for  j:= dimL downto pos do vec[j+1]:= vec[j] ;
+       vec[pos]:= d;
+       dimL := dimL + 1;
+       d:= random(100)
+     end;
+end;
+
+{-----------------------------------------------------------------------------
+IMPRIMIRVECTOR - Imprime todos los nros del vector }
+
+Procedure imprimirVector ( var vec: vector; var dimL: dim );
+var
+   i: dim;
+begin
+     for i:= 1 to dimL do
+         write ('-----');
+     writeln;
+     write (' ');
+     for i:= 1 to dimL do begin
+        if(vec[i] < 9)then
+            write ('0');
+        write(vec[i], ' | ');
+     end;
+     writeln;
+     for i:= 1 to dimL do
+         write ('-----');
+     writeln;
+     writeln;
+End;
+
+//-----------------------------------------------------------------------------------------------------
+
+{Busqueda Binaria}
+
+
+function busquedaBin(v:vector; num,pri,ult,i:integer):boolean;
+
+begin
+    i:=(pri+ult) div 2;
+    if (v[i]=num) then
+        busquedaBin:=true
+    else
+    if (pri = ult) then
+        busquedaBin := false
+    else
+        if (num < v[i]) then
+        begin
+            ult:=i-1;
+            busquedaBin := busquedaBin(v,num,pri,ult,i)
+        end
+        else
+        begin
+            pri:=i+1;
+            busquedaBin := busquedaBin(v,num,pri,ult,i);
+        end;
+end;        
+
+{PROGRAMA PRINCIPAL}
+var
+   v: vector;
+   dimL : dim;
+   num,pri,ult,i:integer;
+
+begin
+    cargarVectorOrdenado(v,dimL);
+    pri:=1;
+    ult:=dimL;
+    writeln('Nros almacenados: ');
+    imprimirVector(v, dimL);
+    Writeln('Ingrese el numero a buscar en el vector: ');
+    Readln(num);
+    if (busquedaBin(v,num,pri,ult,i) = true) then
+        Writeln(num,'se encuentra en el vector.')
+    else
+        Writeln(num,' no se encuentra en el vector');
+end.
