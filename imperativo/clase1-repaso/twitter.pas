9bea9879ed03342252bef8e285cf66a0ff709076
ramirocabral <ramiro.cabral@alu.ing.unlp.edu.ar>
2022-08-11 19:33:00 -0300

    agregando clase 1

--- imperativo/clase1-repaso/twitter.pas
+++ imperativo/clase1-repaso/twitter.pas
@@ -15,7 +15,7 @@ Type
                sig: listaTweets;
      end;
 
-     {Completar agregando aquí las estructuras de datos necesarias en el ejercicio}
+     {Completar agregando aquí las estructuras de dato necesarias en el ejercicio}
      
      //lista de Usuarios
 
@@ -29,12 +29,6 @@ Type
           rtw : boolean;
      end;
 
-     listaUs = ^nodoUs;
-     nodoUs = record
-          dato : usuario;
-          sig : listaUs;
-          tweets : listaTw;
-     end;     
 
      listaTw = ^nodoTw;
      nodoTw = record
@@ -42,21 +36,28 @@ Type
           sig  : listaTw;
      end;
 
-{agregarAdelante - Agrega nro adelante de l}
-Procedure agregarAdelante(var l: listaTweets; t: tweet);
+     listaUs = ^nodoUs;
+     nodoUs = record
+          dato : usuario;
+          sig : listaUs;
+          tweets : listaTw;
+     end;     
+
+{agregarAdelante - Agrega nro adelante de l_ord}
+Procedure agregarAdelante(var l_ord: listaTweets; t: tweet);
 var
    aux: listaTweets;
 begin
      new(aux);
      aux^.dato := t;
-     aux^.sig := l;
-     l:= aux;
+     aux^.sig := l_ord;
+     l_ord:= aux;
 end;
 
 
 
 {crearLista - Genera una lista con tweets aleatorios}
-procedure crearLista(var l: listaTweets);
+procedure crearLista(var l_ord: listaTweets);
 var
    t: tweet;
    texto: string;
@@ -79,7 +80,7 @@ begin
           t.nombreUsuario := v[t.codigoUsuario];
           t.mensaje := texto;
           t.esRetweet := (random(2)=0);
-          agregarAdelante(l, t);
+          agregarAdelante(l_ord, t);
           t.codigoUsuario := random(11);
      end;
 end;
@@ -98,12 +99,12 @@ begin
 end;
 
 
-{imprimirLista - Muestra en pantalla la lista l}
-procedure imprimirLista(l: listaTweets);
+{imprimirLista - Muestra en pantalla la lista l_ord}
+procedure imprimirLista(l_ord: listaTweets);
 begin
-     while (l <> nil) do begin
-          imprimir(l^.dato);
-          l:= l^.sig;
+     while (l_ord <> nil) do begin
+          imprimir(l_ord^.dato);
+          l_ord:= l_ord^.sig;
      end;
 end;
 
@@ -147,30 +148,115 @@ end;
 
 {----------------FIN DE SE DISPONE----------------------}
 
+//agregar usuario a la lista principal
 
+procedure agregarDetrasUsuario (var lUs,actNodo:listaUs; data:usuario);
 
-procedure agregarDetrasUsuario (var lUs)
+var nue:listaUs; act:listaUs;
+
+begin
+     new(nue);
+     nue^.dato:=data;
+     nue^.sig:=nil;
+     nue^.tweets:=nil;
+     if (lUs = nil) then
+     begin
+          lUs := nue;
+     end
+     else
+     begin
+          act := lUs;
+          while (act^.sig <> nil) do
+          begin
+               act := act^.sig;
+          end;
+          act^.sig:=nue;
+     end;
+     actNodo:=nue;
+end;
+
+//agregar tweets a la lista de cada usuario
+
+procedure agregarDelanteTweet (var lT:listaTw; data:tw);
+
+var nue:listaTw;
+
+begin
+     new(nue);
+     nue^.dato:=data;
+     nue^.sig:=lT;
+     lT:=nue;
+end;
+
+//cargar usuarios y tweets
+
+procedure nuevaLista (l_ord:listaTweets; var lUs:listaUs);
+
+var  usAct:string; 
+     usAgregado:usuario;
+     twAgregado:tw;
+     actNodo:listaUs;
+begin
+     actNodo:=nil;
+     while (l_ord <> nil) do
+     begin
+          usAct := l_ord^.dato.nombreUsuario;
+          usAgregado.nombre := usAct;
+          usAgregado.codigo := l_ord^.dato.codigoUsuario;
+          agregarDetrasUsuario(lUs,actNodo,usAgregado);
+          while (l_ord <> nil) and (l_ord^.dato.nombreUsuario = usAct) do
+          begin
+               twAgregado.msj:=l_ord^.dato.mensaje;
+               twAgregado.rtw:=l_ord^.dato.esRetweet;
+               agregarDelanteTweet(actNodo^.tweets,twAgregado);
+               l_ord:=l_ord^.sig;
+          end; 
+     end;
+end;
+
+procedure imprimirListas (lUs:listaUs);
+
+var actTw:listaTw;
+
+begin
+     while (lUs <> nil) do
+     begin
+          Writeln('Usuario: ',lUs^.dato.nombre);
+          actTw:=lUs^.tweets;
+          while (actTw <> nil) do
+          begin
+               Write(actTw^.dato.msj,' - ');
+               actTw := actTw^.sig;
+          end;
+          lUs := lUs^.sig;
+          Writeln('');
+     end; 
+end;
 
 
 var
-   l, l_ordenada: listaTweets;
+   l_ord, l_ordenada: listaTweets;
    lUs : listaUs;
 begin
      Randomize;
 
-     l:= nil;
-     crearLista(l);
+     l_ord:= nil;
+     crearLista(l_ord);
      writeln ('Lista generada: ');
-     imprimirLista(l);
+     imprimirLista(l_ord);
 
      {Se crea la estructura ordenada}
      l_ordenada:= nil;
-     generarNuevaEstructura(l,l_ordenada);
+     generarNuevaEstructura(l_ord,l_ordenada);
      writeln ('Lista ordenada: ');
      imprimirLista(l_ordenada);
 
      {Completar el programa}
      lUs := nil;
+     Writeln('Creando nueva lista: ');
+     nuevaLista(l_ordenada,lUs);
+     Writeln('Imprimiendo nueva lista: ');
+     imprimirListas(lUs);
      writeln('Fin del programa');
      readln;
 end.
